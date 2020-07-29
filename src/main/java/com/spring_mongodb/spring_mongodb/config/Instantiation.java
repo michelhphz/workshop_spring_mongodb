9b7d39e30685f64e9afd9cf8adfe7b350d492a80
michelPech <michelhphz@hotmail.com>
2020-07-28 21:43:42 -0300

    Criando entidade POST com User aninhado

--- src/main/java/com/spring_mongodb/spring_mongodb/config/Instantiation.java
+++ src/main/java/com/spring_mongodb/spring_mongodb/config/Instantiation.java
@@ -1,8 +1,12 @@
 package com.spring_mongodb.spring_mongodb.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
+import com.spring_mongodb.spring_mongodb.domain.Post;
 import com.spring_mongodb.spring_mongodb.domain.User;
+import com.spring_mongodb.spring_mongodb.repositories.PostRepository;
 import com.spring_mongodb.spring_mongodb.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,16 +19,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
         
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
     
 }
