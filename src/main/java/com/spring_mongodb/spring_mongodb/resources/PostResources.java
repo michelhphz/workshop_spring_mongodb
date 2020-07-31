2def707d6a00e151db2e13fc318b6adc4d01f862
michelPech <michelhphz@hotmail.com>
2020-07-30 21:08:49 -0300

    Obtendo um post por id

---
+++ src/main/java/com/spring_mongodb/spring_mongodb/resources/PostResources.java
@@ -0,0 +1,25 @@
+package com.spring_mongodb.spring_mongodb.resources;
+
+import com.spring_mongodb.spring_mongodb.domain.Post;
+import com.spring_mongodb.spring_mongodb.services.PostService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResources {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }     
+}
---
+++ src/main/java/com/spring_mongodb/spring_mongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.spring_mongodb.spring_mongodb.services;
+
+import java.util.Optional;
+
+import com.spring_mongodb.spring_mongodb.domain.Post;
+import com.spring_mongodb.spring_mongodb.repositories.PostRepository;
+import com.spring_mongodb.spring_mongodb.services.exceptions.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }    
+
+}
